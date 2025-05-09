1c11fcf0a09083902911c3e5685b7410c229222d
brugaate <gabetrau@gmail.com>
2025-05-09 00:26:05 -0500

    don't use fancy tab bar

--- wezterm.lua
+++ wezterm.lua
@@ -2,5 +2,8 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.color_scheme = 'MaterialOcean'
+config.use_fancy_tab_bar = false
+
+config.leader = { key = 'w', mods = 'CTRL', timeout_milliseconds = 1000 }
 
 return config
